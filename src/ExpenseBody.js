763f379c1c86111f97735cefb9ba8507d1c12c51
Honshu11 <egonza67@yahoo.com>
2021-06-28 17:35:18 -0700

    handle submit?

--- src/ExpenseBody.js
+++ src/ExpenseBody.js
@@ -2,24 +2,26 @@ import React from "react";
 
 function ExpenseBody(props){
     //TODO: Create an array with data and use the map method to iterate through the array and pass it to the table body.
-    const expenseData = [
-        {
-            date: date,
-            description: description,
-            amount: amount,
-            location: location,
-        }
-    ]
-
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        const expenseData = [
+            {
+                date,
+                description,
+                amount,
+                location,
+            }
+        ]
+    }
     
     return(
         <div>
             <tbody className="table-body">
                 <tr>
-                    <td>{expenseData.date}</td>
-                    <td>{expenseData.description}</td>
-                    <td>{expenseData.amount}</td>
-                    <td>{expenseData.location}</td>
+                    <td>{props.expenseData.date}</td>
+                    <td>{props.expenseData.description}</td>
+                    <td>{props.expenseData.amount}</td>
+                    <td>{props.expenseData.location}</td>
                 </tr>
             </tbody>
         </div>
