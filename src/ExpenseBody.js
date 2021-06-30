9cea16496cdb9220e79e8dbeec4f5742e3a23851
Honshu11 <egonza67@yahoo.com>
2021-06-30 16:14:46 -0700

    added question regarding props and passing them through

--- src/ExpenseBody.js
+++ src/ExpenseBody.js
@@ -2,30 +2,29 @@ import React from "react";
 
 function ExpenseBody(props){
     //TODO: Create an array with data and use the map method to iterate through the array and pass it to the table body.
-    const handleSubmit =(e) => {
-        e.preventDefault();
-        const expenseData = [
-            {
-                date,
-                description,
-                amount,
-                location,
-            }
-        ]
-    }
-    
-    return(
-        <div>
-            <tbody className="table-body">
-                <tr>
-                    <td>{props.expenseData.date}</td>
-                    <td>{props.expenseData.description}</td>
-                    <td>{props.expenseData.amount}</td>
-                    <td>{props.expenseData.location}</td>
-                </tr>
-            </tbody>
-        </div>
-    )
-};
+        // const expenseData = [
+        //     {
+        //         date,
+        //         description,
+        //         amount,
+        //         location,
+        //     }
+        // ]
+
+        //TODO: How do I aquire the information being passed from the expenseData array to be passed to the table rendered on the web browser.
+        // onClick button in the App.js? map method? 
+        return(
+            <div>
+                <tbody className="table-body">
+                    <tr>
+                        <td>{props.expenseData.date}</td>
+                        <td>{props.expenseData.description}</td>
+                        <td>{props.expenseData.amount}</td>
+                        <td>{props.expenseData.location}</td>
+                    </tr>
+                </tbody>
+            </div>
+        )
+    }   
 
 export default ExpenseBody;
